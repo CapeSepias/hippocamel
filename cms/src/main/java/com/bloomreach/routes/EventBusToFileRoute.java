d04e56f961c4356405ef937414271b854481e44e
Baris Vural <bariscan.vural@bloomreach.com>
2018-05-07 16:45:05 +0200

    Add minimal camel setup to cms webapp

---
+++ cms/src/main/java/com/bloomreach/routes/EventBusToFileRoute.java
@@ -0,0 +1,20 @@
+package com.bloomreach.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import net.sf.json.JSON;
+
+@Component
+public class EventBusToFileRoute extends RouteBuilder {
+    @Override
+    public void configure() {
+        //Subscribe publish/depublish events as JSON from HippoEventBus.
+        from("hippoevent:?category=workflow&action=publish,depublish")
+                .convertBodyTo(String.class)
+                .convertBodyTo(JSON.class)
+                .to("log:com.bloomreach.kafka?level=INFO")
+                // Store the JSON string to a file in the 'inbox' folder
+                .to("file:inbox?autoCreate=true&charset=utf-8");
+    }
+}
